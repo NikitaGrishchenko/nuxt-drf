bb7c7ca4715d1a92e5d3aca5fbb434a09ee96add
Nikita <grishchenkonikita@mail.ru>
2021-08-17 09:32:44 +0300

    configured jwt-auth on backend

--- backend/apps/base/v1/routers.py
+++ backend/apps/base/v1/routers.py
@@ -1,38 +1,18 @@
-# from django.urls import path
-# from rest_framework_simplejwt.views import (
-#     TokenCookieDeleteView,
-#     TokenVerifyView,
-# )
 
 from apps.base.v1.views import UserListView
-
 # from .views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 app_name = "base"
 
 urlpatterns = [
-    path("test/", UserListView.as_view(), name="user-list"),
-    # path(
-    #     "token/",
-    #     TokenObtainPairView.as_view(
-    #         token_refresh_view_name="api:v1:auth:token_refresh"
-    #     ),
-    #     name="token_obtain_pair",
-    # ),
-    # path(
-    #     "refresh/",
-    #     TokenRefreshView.as_view(
-    #         token_refresh_view_name="api:v1:auth:token_refresh"
-    #     ),
-    #     name="token_refresh",
-    # ),
-    # path("verify/", TokenVerifyView.as_view(), name="token_verify"),
-    # path(
-    #     "delete/",
-    #     TokenCookieDeleteView.as_view(
-    #         token_refresh_view_name="api:v1:auth:token_refresh"
-    #     ),
-    #     name="token_delete",
-    # ),
+    path("users/", UserListView.as_view(), name="user-list"),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- backend/core/settings/common.py
+++ backend/core/settings/common.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from .environment import env
@@ -36,13 +37,18 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "webpack_loader",
     "rest_framework",
+    # 'rest_framework.authtoken',
+    'rest_framework_simplejwt',
+    'corsheaders',
     "drf_yasg",
     "widget_tweaks",
     "apps.base",
     # "core.api",
 ]
 
+
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -130,9 +136,41 @@ MEDIA_URL = "/media/"
 
 # REST API
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ]
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': env('SECRET_KEY'),
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
